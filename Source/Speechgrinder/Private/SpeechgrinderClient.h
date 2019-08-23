fad2466c042c6aaf76e2d855f2f109532a8af18a
Väinö Järvelä <vaino@speechgrinder.com>
2019-08-23 09:15:58 +0300

    Move SpeechgrinderClient to it's own file

---
+++ Source/Speechgrinder/Private/SpeechgrinderClient.h
@@ -0,0 +1,68 @@
+#pragma once
+
+#include "CoreMinimal.h"
+
+#include <memory>
+#include <string>
+
+#include "IncludeBegin.h"
+THIRD_PARTY_INCLUDES_START
+#include "grpc++/grpc++.h"
+#include "sg.grpc.pb.h"
+THIRD_PARTY_INCLUDES_END
+#include "IncludeEnd.h"
+
+using namespace speechgrinder::sgapi::v1;
+
+// GRPC threaded client
+class SPEECHGRINDER_API SpeechgrinderClient : public FRunnable
+{
+public:
+	SpeechgrinderClient(const std::string& Address, const std::string& DeviceId, const std::string& AppId, const std::string& LanguageCode);
+	bool Write(const SluRequest& Request);
+	bool Read(SluResponse& OutResponse);
+	bool HasError() const
+	{
+		return bHasError;
+	}
+
+	virtual bool Init() override;
+	virtual uint32 Run() override;
+	virtual void Exit() override;
+	virtual void Stop() override;
+
+private:
+	// GRPC abstractions
+	bool CanWrite();
+	bool CanRead();
+	void StreamWrite(const SluRequest& Request);
+	void StreamRead(SluResponse* OutResponse);
+	void HandleTag(void* Tag);
+	void SetError(const FString& Message);
+
+private:
+	std::string Address;
+	std::string DeviceId;
+	std::string AppId;
+	std::string LanguageCode;
+	std::shared_ptr<grpc::ChannelCredentials> Credentials;
+	std::shared_ptr<grpc::Channel> Channel;
+	std::unique_ptr<Slu::Stub> SluStub;
+	std::unique_ptr<grpc::ClientAsyncReaderWriterInterface<SluRequest, SluResponse>> Stream;
+
+	// GRPC shared state to be able to cleanup memory used in Run() at Exit()
+	// Otherwise not meant to be used outside of Run()
+	grpc::ClientContext Context;
+	grpc::CompletionQueue CompletionQueue;
+	uintptr_t Seq{ 0 };
+	uintptr_t LastWriteSeq{ 0 };
+	uintptr_t LastReadSeq{ 0 };
+	bool bWriteAllowed{ true };
+	bool bReadAllowed{ true };
+	SluResponse Response;
+
+	bool bIsRunning{ true };
+	bool bHasError{ false };
+	TQueue<SluRequest, EQueueMode::Mpsc> RequestQueue;
+	TQueue<SluResponse, EQueueMode::Mpsc> ResponseQueue;
+};
--- Source/Speechgrinder/Public/Speechgrinder.h
+++ Source/Speechgrinder/Public/Speechgrinder.h
@@ -7,75 +7,13 @@
 #include "LogMacros.h"
 #include "Containers/Queue.h"
 
-#include <memory>
-#include <string>
-
-#include "IncludeBegin.h"
-THIRD_PARTY_INCLUDES_START
-#include "grpc++/grpc++.h"
-#include "sg.grpc.pb.h"
-THIRD_PARTY_INCLUDES_END
-#include "IncludeEnd.h"
+#include "SpeechgrinderClient.h"
 
 #include "Speechgrinder.generated.h"
 
 DECLARE_LOG_CATEGORY_EXTERN(LogSG, Log, All);
 
-using namespace speechgrinder::sgapi::v1;
 using namespace Audio;
-
-// GRPC threaded client
-class SPEECHGRINDER_API SpeechgrinderClient : public FRunnable
-{
-public:
-    SpeechgrinderClient(const std::string& Address, const std::string& DeviceId, const std::string& AppId, const std::string& LanguageCode);
-    bool Write(const SluRequest& Request);
-    bool Read(SluResponse& OutResponse);
-    bool HasError() const
-    {
-        return bHasError;
-    }
-    
-    virtual bool Init() override;
-    virtual uint32 Run() override;
-    virtual void Exit() override;
-    virtual void Stop() override;
-
-private:
-	// GRPC abstractions
-	bool CanWrite();
-	bool CanRead();
-	void StreamWrite(const SluRequest& Request);
-	void StreamRead(SluResponse* OutResponse);
-	void HandleTag(void* Tag);
-	void SetError(const FString& Message);
-
-private:
-    std::string Address;
-    std::string DeviceId;
-    std::string AppId;
-    std::string LanguageCode;
-    std::shared_ptr<grpc::ChannelCredentials> Credentials;
-	std::shared_ptr<grpc::Channel> Channel;
-    std::unique_ptr<Slu::Stub> SluStub;
-    std::unique_ptr<grpc::ClientAsyncReaderWriterInterface<SluRequest, SluResponse>> Stream;
-
-    // GRPC shared state to be able to cleanup memory used in Run() at Exit()
-    // Otherwise not meant to be used outside of Run()
-    grpc::ClientContext Context;
-    grpc::CompletionQueue CompletionQueue;
-    uintptr_t Seq{0};
-    uintptr_t LastWriteSeq{0};
-    uintptr_t LastReadSeq{0};
-    bool bWriteAllowed{true};
-    bool bReadAllowed{true};
-    SluResponse Response;
-
-    bool bIsRunning{true};
-    bool bHasError{false};
-    TQueue<SluRequest, EQueueMode::Mpsc> RequestQueue;
-    TQueue<SluResponse, EQueueMode::Mpsc> ResponseQueue;
-};
 
 class ISpeechAudioCallback
 {
@@ -248,9 +186,7 @@ public:
     
 	/** Returns true if connected to Speechgrinder */
 	UFUNCTION(BlueprintCallable)
-	bool isConnected() const {
-		return Client.IsValid() && !Client->HasError();
-	}
+	bool isConnected() const;
 
 private:
 	FString LastUtteranceId;
